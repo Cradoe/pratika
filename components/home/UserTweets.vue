02139c960765112e4ee78f39ea76b2cf41640a71
Cradoe <me@ismailobadimu.com>
2022-04-21 06:12:02 +0100

    fix: handled user tweets responsiveness

--- components/home/UserTweets.vue
+++ components/home/UserTweets.vue
@@ -1,14 +1,14 @@
 <template>
     <section class="px-4 lg:px-0 lg:pl-16 pb-16 dark:bg-secondary/95 ">
         <div
-            class="bg-secondary dark:bg-[#1E293B] dark:shadow-2xl p-10 lg:pl-16  rounded-bl-3xl flex flex-col lg:flex-row gap-x-28 gap-y-16">
+            class="bg-secondary dark:bg-[#1E293B] dark:shadow-2xl p-10 lg:pl-16  xl:rounded-bl-3xl flex flex-col lg:flex-row gap-x-28 gap-y-10">
 
-            <div class="flex justify-center items-start flex-col gap-10 lg:w-5/12 lg:order-2">
-                <p class="text-[#F8FAFC] text-2xl">
+            <div class="flex justify-center items-start flex-col gap-y-4 gap-x-10 lg:w-5/12 lg:order-2">
+                <p class="text-[#F8FAFC] lg:text-2xl">
                     Professionals across the globe won't be talking about us if we aren't the the best lorem ipsum
                 </p>
-                <router-link to="/about" class="text-primary hover:text-primary/80">
-                    <svg width="128" height="23" class="hidden lg:inline mr-3" viewBox="0 0 128 23" fill="none"
+                <router-link to="/about" class="text-sm lg:text-base text-primary hover:text-primary/80">
+                    <svg width="128" height="23" class="inline mr-3 w-12 lg:w-auto" viewBox="0 0 128 23" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
                         <path
                             d="M127.061 12.2647C127.646 11.6789 127.646 10.7292 127.061 10.1434L117.515 0.597439C116.929 0.011652 115.979 0.011652 115.393 0.597439C114.808 1.18323 114.808 2.13297 115.393 2.71876L123.879 11.204L115.393 19.6893C114.808 20.2751 114.808 21.2249 115.393 21.8106C115.979 22.3964 116.929 22.3964 117.515 21.8106L127.061 12.2647ZM0 12.704H126V9.70404H0V12.704Z"
@@ -26,12 +26,12 @@
                 </router-link>
             </div>
 
-            <div class="lg:w-7/12 items-start grid xl:grid-cols-2 gap-10">
+            <div class="lg:w-7/12 items-start grid md:grid-cols-2 md:gap-4 xl:gap-10">
                 <!--  divided four tweets into two subsections for proper UI alignment  -->
-                <div v-for="(                                                   subTweetsId, index                                                   ) in                                                   testimonialTweetsId                                                   "
+                <div v-for="(                                                                                subTweetsId, index                                                                                ) in                                                                                testimonialTweetsId                                                                                "
                     :key=" index " class="grid  gap-4">
                     <Tweet
-                        v-for="(                                                 tweetId, j                                                 ) in                                                 subTweetsId                                                "
+                        v-for="(                                                                              tweetId, j                                                                              ) in                                                                              subTweetsId                                                                             "
                         :id=" tweetId " :key=" j " error-message="This tweet could not be loaded">
                         <div class="h-64 border border-[#eee]"></div>
                     </Tweet>
