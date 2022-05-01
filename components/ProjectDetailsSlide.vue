030d60f1463b3e9e50f51ea755532446861df65a
Cradoe <me@ismailobadimu.com>
2022-05-01 23:59:25 +0100

    ci: modified project details slide

--- components/ProjectDetailsSlide.vue
+++ components/ProjectDetailsSlide.vue
@@ -1,18 +1,61 @@
 <template>
-    <div class="fixed top-0 left-0 bg-black w-[100vw] h-[100vh] z-30 flex justify-end">
+    <div class="fixed top-0 left-0 bg-black/30 w-[100vw] h-[100vh] z-30 flex justify-end">
         <div class="bg-white h-full w-[50vw] overflow-y-auto">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam quo ducimus nihil iusto iste sapiente nobis
-            deleniti corporis in, omnis ullam harum quam mollitia eligendi a eaque laborum perferendis? Facere.
+
+            <ul class="border  border-t-0 xl:border-t border-dotted border-primary w-10/12 mx-auto h-auto text-sm mt-5">
+
+                <li class="px-4 py-4 border-t-0 xl:border-t border border-dotted border-b-primary">
+                    <span class="font-semibold">Project Name: </span> {{ project.title }}
+                </li>
+                <li v-for="(                   feature, index                   ) in                    project.features                   "
+                    :key=" index " class="px-4 py-4 border border-b-primary">
+                    <span class="font-semibold">{{ feature.title }}:</span>
+                    <span v-if=" Array.isArray( feature.value ) ">
+                        {{ feature.value.join( " , " ) }}
+                    </span>
+                    <span v-else>
+                        {{ feature.value }}
+                    </span>
+                </li>
+                <li v-for="(                   info, index                   ) in                    project.additionalInfo                   "
+                    :key=" index " class="px-4 py-4 border border-b-primary">
+                    <span class="font-semibold">{{ info.title }}:</span>
+                    <span v-if=" Array.isArray( info.value ) ">
+                        {{ info.value.join( " , " ) }}
+                    </span>
+                    <span v-else>
+                        {{ info.value }}
+                    </span>
+                </li>
+
+                <li class="px-4 py-4 border border-b-primary">
+                    <span class="font-semibold">Relevant Resources</span>
+
+                    <div v-for="(                   resource, index                   ) in                    project.resources                   "
+                        :key=" index ">
+                        <span>( {{ index + 1 }} )</span>
+                        <a :href=" resource.link " class="text-primary font-bold">{{ resource.title }} </a>
+                    </div>
+
+                </li>
+                <li class="px-4 py-4">
+                    <span class="font-semibold">Description</span>
+                    <p>{{ project.description }}</p>
+                </li>
+                <li class="px-4 py-7 flex flex-col md:flex-row justify-between gap-y-4 gap-x-10">
+                    <slot name="cta-button"></slot>
+                </li>
+            </ul>
         </div>
     </div>
 </template>
 <script>
-    export default {
-        props: {
-            project: {
-                type: Object,
-                required: true
-            }
+export default {
+    props: {
+        project: {
+            type: Object,
+            required: true
         }
     }
+}
 </script>
--- components/dashboard/Projects.vue
+++ components/dashboard/Projects.vue
@@ -1,71 +1,106 @@
 <template>
     <section class="lg:py-8">
 
-        <DashboardProjectCard v-for="(project, index) in recommendedProjects" :key="index" :number="index+1"
-            :project="project">
+        <DashboardProjectCard v-for="(          project, index          ) in           recommendedProjects          "
+            :key=" index " :number=" index + 1 " :project=" project ">
 
             <template #cta-button>
                 <Button mode="secondary" class="md:w-6/12">View Details</Button>
                 <Button class="md:w-6/12">Start Project</Button>
             </template>
-
             <template #details-slide>
-                <ProjectDetailsSlide :project="project" />
+                <ProjectDetailsSlide :project=" project " />
             </template>
+
 
         </DashboardProjectCard>
 
     </section>
 </template>
 <script>
-    export default {
-        name: "NuxtProjects",
-        data () {
-            return {
-                // recommendedProjects should be fetched from server when API is ready.
-                recommendedProjects: [
-                    {
-                        id: "001",
-                        title: "Remote Job Hero Header - Web Design",
-                        image: "https://cdn.dribbble.com/users/3894633/screenshots/17067293/media/c6925eaba9d7fc55896b37677d5aad8b.png",
-                        features: [
-                            {
-                                title: "Attibution",
-                                value: [ "Ismail Obadimu", "Adewale Ayuba" ]
-                            },
-                            {
-                                title: "Tools/Stacks",
-                                value: [
-                                    "Git",
-                                    "React JS",
-                                    "Tailwind CSS",
-                                    "Jest" ]
-                            }
-                        ],
-                        description: "This is a Remote Job Hero Header - Web Design. Hope you guys will like it. Let me know your thought's on that. Your feedback and appreciation is always welcome."
-                    },
-                    {
-                        id: "002",
-                        title: "Remote Job Hero Header 2 - Web Design",
-                        image: "https://cdn.dribbble.com/users/2151039/screenshots/15422478/media/db41ab4b5ca5649fdee409900ea11118.jpg",
-                        features: [
-                            {
-                                title: "Attibution",
-                                value: [ "Ismail Obadimu", "Adewale Ayuba" ]
-                            },
-                            {
-                                title: "Tools/Stacks",
-                                value: [
-                                    "Git",
-                                    "React JS",
-                                    "Tailwind CSS",
-                                    "Jest" ]
-                            }
-                        ],
-                        description: "This is a Remote Job Hero Header - Web Design. Hope you guys will like it. Let me know your thought's on that. Your feedback and appreciation is always welcome."
-                    }
-                ]
-            }
+export default {
+    name: "NuxtProjects",
+    data () {
+        return {
+            // recommendedProjects should be fetched from server when API is ready.
+            recommendedProjects: [
+                {
+                    id: "001",
+                    title: "Remote Job Hero Header - Web Design",
+                    image: "https://cdn.dribbble.com/users/3894633/screenshots/17067293/media/c6925eaba9d7fc55896b37677d5aad8b.png",
+                    features: [
+                        {
+                            title: "Attibution",
+                            value: [ "Ismail Obadimu", "Adewale Ayuba" ]
+                        },
+                        {
+                            title: "Tools/Stacks",
+                            value: [
+                                "Git",
+                                "React JS",
+                                "Tailwind CSS",
+                                "Jest" ]
+                        }
+                    ],
+                    additionalInfo: [
+                        {
+                            title: "Attibution",
+                            value: [ "Ismail Obadimu", "Adewale Ayuba" ]
+                        },
+                        {
+                            title: "Tools/Stacks",
+                            value: [
+                                "Git",
+                                "React JS",
+                                "Tailwind CSS",
+                                "Jest" ]
+                        }
+                    ],
+                    description: "This is a Remote Job Hero Header - Web Design. Hope you guys will like it. Let me know your thought's on that. Your feedback and appreciation is always welcome."
+                },
+                {
+                    id: "002",
+                    title: "Remote Job Hero Header 2 - Web Design",
+                    image: "https://cdn.dribbble.com/users/2151039/screenshots/15422478/media/db41ab4b5ca5649fdee409900ea11118.jpg",
+                    features: [
+                        {
+                            title: "Attibution",
+                            value: [ "Ismail Obadimu", "Adewale Ayuba" ]
+                        },
+                        {
+                            title: "Tools/Stacks",
+                            value: [
+                                "Git",
+                                "React JS",
+                                "Tailwind CSS",
+                                "Jest" ]
+                        }
+                    ],
+                    additionalInfo: [
+                        {
+                            title: "Enrollments",
+                            value: '8900'
+                        },
+                        {
+                            title: "Average Performance",
+                            value: "89%"
+                        },
+                        {
+                            title: "Project Estimated Duration",
+                            value: "2days 3hours"
+                        }
+                    ],
+                    resources: [ {
+                        title: "File A-z",
+                        link: "https://ismailobadimu.com"
+                    }, {
+                        title: "File A-z",
+                        link: "https://ismailobadimu.com"
+                    } ],
+                    description: "This is a Remote Job Hero Header - Web Design. Hope you guys will like it. Let me know your thought's on that. Your feedback and appreciation is always welcome."
+                }
+            ]
         }
     }
+}
 </script>
