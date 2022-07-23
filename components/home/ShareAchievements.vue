fc6b84442f2aaa84ec9d1f85455b4e1d9177a144
Cradoe <me@ismailobadimu.com>
2022-07-23 10:35:44 +0100

    refactor: modified selection icon and reduced empty spaces

--- components/home/ShareAchievements.vue
+++ components/home/ShareAchievements.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="dark:bg-dark-900 py-16 mb-4">
+    <section class="dark:bg-dark-900 pb-16 mb-4">
         <HomeNumberedSectionHeading number=" 2 " heading="Share your Achievements"
             description="Accomplishment stories, a way to learn in public can provide that critical information, proving that you're credible and good at what you do." />
 
@@ -8,7 +8,10 @@
                 class="grid md:grid-cols-2 xl:grid-cols-3 gap-y-16 md:gap-x-20 xl:gap-x-40 mx-6 md:mx-16 xl:mx-20 mt-12 xl:mt-16">
                 <div v-for="(           feature, index           ) in            features           " :key=" index "
                     class="rounded-md xl:rounded-3xl feature py-8 md:py-16 px-4 md:px-8 xl:px-16 text-center hover:shadow-2xl transition-all duration-200 dark:bg-dark-700">
-                    <nuxt-img :src=" feature.image " class="w-24 mx-auto" />
+                    
+                    <nuxt-img :src=" feature.image " class="w-24 mx-auto " :class="feature.imageDark ? 'block dark:hidden' : 'block'" />
+
+                    <nuxt-img v-if="feature.imageDark" :src=" feature.imageDark" class="w-24 mx-auto hidden dark:block" />
                     <p class="text-secondary dark:text-white/90 text-sm mt-3">{{ feature.caption }} </p>
                 </div>
             </div>
@@ -26,6 +29,7 @@ export default {
             features: [
                 {
                     image: '/selection-icon.png',
+                    imageDark: '/selection-icon-white.png',
                     caption: 'Pick a project, and see how you can improve your skills.'
                 },
                 {
